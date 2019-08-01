19e4cae7cb44b28d3deb9fa2bb3f602e984d4031
katiejolly <katherine.jolly@metc.state.mn.us>
2019-08-01 15:29:30 -0500

    population lines map

--- R/make_maps.R
+++ R/make_maps.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(sf)
 library(LaCroixColoR)
 library(showtext)
+library(rgdal)
 
 font_add_google("Lato", bold.wt = 400)
 showtext_auto()
@@ -111,4 +112,71 @@ ggplot(states_mercator) +
   geom_sf_() +
   theme_minimal()
 
+########### POPULATION LINES
+
+library(sp)
+library(rgdal)
+library(reshape)
+library(ggplot2)
+library(maptools)
+library(raster)
+library(rgeos) #create a range standardisation function
+range01 <- function(x){(x-min(x))/(max(x)-min(x))}
+
+
+
+input<-raster("data/gpw_v4_population_count_rev11_2020_2pt5_min.tif") # the latest data come as a Tiff so you will need to tweak.
+
+
+
+proj4string(input) = CRS("+init=epsg:4326")
+
+midwest <- st_transform(midwest_state_plane, projection(input)) %>%
+  filter(str_detect(ZONE, "MN"))
+
+pop_masked = mask(input, midwest)
+
+pop_df <- as(pop_masked, 'SpatialGridDataFrame')
+
+
+values<-melt(pop_df)
+names(values)<- c("pop", "x", "y")
+
+#Rescale the values. This is to ensure that you can see the variation in the data
+values$pop_st<-range01(values$pop)*0.20
+values$x_st<-range01(values$x)
+values$y_st<-range01(values$y)
+
+#Switch off various ggplot things
+
+xquiet<- scale_x_continuous("", breaks=NULL)
+yquiet<-scale_y_continuous("", breaks=NULL)
+quiet<-list(xquiet, yquiet)
+
+#Add 180 to the latitude to remove negatives in southern hemisphere, then order them.
+values$ord<-values$y+180
+values_s<- values[order(-values$ord),]
+
+#Create an empty plot called p
+p<-ggplot()
+
+#This loops through each line of latitude and produced a filled polygon that will mask out the lines beneath and then plots the paths on top.The p object becomes a big ggplot2 plot.
+for (i in unique(values_s$ord))
+{
+  p<-p+geom_polygon(data=values_s[values_s$ord==i,],aes(x_st, pop_st+y_st,group=y_st), size=0.2, fill="#F9E7E0", col="#F9E7E0")+ geom_path(data=values_s[values_s$ord==i,],aes(x_st, pop_st+y_st,group=y_st),size=0.3, lineend="round", color = "#B75D69")
+}
+
+font_add_google("Quicksand", bold.wt = 400)
+showtext_auto()
+
+
+
+p +theme(panel.background = element_rect(fill='#F9E7E0',colour='#F9E7E0'),
+         plot.background = element_rect(fill = "#F9E7E0"))+quiet +
+  annotate("text", y = 0.25, x = 0.64, label = "TWIN CITIES", family = "Quicksand", color = "#B75D69", size = 6) +
+  annotate("text", y = 0.56, x = 0.74, label = "DULUTH", family = "Quicksand", color = "#B75D69", size = 6) +
+  annotate("rect", ymin = .34, ymax = .35, xmin = .29, xmax = .35, fill = "#F9E7E0", color = "#F9E7E0") +
+  annotate("text", y = .345, x = .32, label = "ST CLOUD", family = "Quicksand", color = "#B75D69", size = 6)
+
+ggsave("images/mn_poplines_text.png")
 
